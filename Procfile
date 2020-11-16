0862083e0f2e30f0dff9e460364ce6e9260faddf
smdedar <thededar@gmail.com>
2020-11-16 22:46:53 +0600

    fix

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn app:apppip freeze > requirements.txt
+web: gunicorn --bind 0.0.0.0:$PORT app:app
--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 import apidata
 
 app = Flask(__name__)
-app.config["DEBUG"] = True
+#app.config["DEBUG"] = True
 
 @app.route('/')
 def index():
